17567337e47b2b635b4bd14b3b81a487bc330400
Md. Minhazul Haque <1847242+mdminhazulhaque@users.noreply.github.com>
2021-02-28 23:08:46 +0600

    Intentional mistake

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+module.exports =
   entry: './app.js',
   mode: 'development',
   plugins: [
